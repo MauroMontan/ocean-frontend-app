86a649a7445a481f90ba22b70c9a945ee1504a43
mauro <mauroanastacio@outlook.es>
2022-05-13 19:05:12 -0500

    store and router configured

---
+++ src/router.ts
@@ -0,0 +1,20 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [
+    {
+        path: "/",
+        name: "home",
+        component: () => import("./views/home_view.vue"),
+    },
+];
+
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+}
+);
+
+export default router;
+
